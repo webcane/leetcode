a11f16ad818356038023cf59213766bf5c3bacb7
Mikhail Niedre <mikhail.niedre@quantori.com>
2026-02-04 21:51:47 +0500

    add singleElement

--- src/main/java/cane/brothers/problems/single_element/Solution.java
+++ src/main/java/cane/brothers/problems/single_element/Solution.java
@@ -7,52 +7,71 @@ import java.util.stream.Collector;
 import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 
+/**
+ * Given a non-empty array of integers nums, every element appears twice except for one. Find that single one.
+ * You must implement a solution with a linear runtime complexity and use only constant extra space.
+ * Constraints:
+ * 1 <= nums.length <= 3 * 104
+ * -3 * 104 <= nums[i] <= 3 * 104
+ * Each element in the array appears twice except for one element which appears only once.
+ */
 public class Solution {
 
-  public int getSingleElement(int[] nums) {
+    public int getSingleElement(int[] nums) {
 //    return classic(nums);
-    return streams(nums);
-  }
+//    return streams(nums);
+    return hashMap(nums);
+//        return xor(nums);
+    }
 
-  private int streams(int[] nums) {
-    Map<Integer, Integer> map = new HashMap<>();
-
-    IntStream.of(nums)
-        .forEach(num -> map.compute(num, (key, val) -> computeOccurrence(val)));
-
-    return map.entrySet().stream()
-        .filter(e -> e.getValue() == 1)
-        .map(e -> e.getKey())
-        .collect(toSingleton());
-  }
-
-  private Integer computeOccurrence(Integer val) {
-    return (val == null ? 1 : ++val);
-  }
-
-  public static <T> Collector<T, ?, T> toSingleton() {
-    return Collectors.collectingAndThen(
-        Collectors.toList(),
-        list -> {
-          if (list.size() != 1) {
-            throw new IllegalStateException("there is no single element");
-          }
-          return list.get(0);
+    private int xor(int[] nums) {
+        int result = 0;
+        for (int num : nums) {
+            // magic
+            result ^= num;
         }
-    );
-  }
-
-  private int classic(int[] nums) {
-    Map<Integer, Integer> map = new HashMap<>();
-    for (int n : nums) {
-      map.compute(n, (key, val) -> (val == null) ? 1 : ++val);
+        return result;
     }
 
-    for (Entry<Integer, Integer> entry : map.entrySet()) {
-      if (entry.getValue() == 1) {
-        return entry.getKey();
-      }
+    public static <T> Collector<T, ?, T> toSingleton() {
+        return Collectors.collectingAndThen(
+                Collectors.toList(),
+                list -> {
+                    if (list.size() != 1) {
+                        throw new IllegalStateException("there is no single element");
+                    }
+                    return list.get(0);
+                }
+        );
     }
-    throw new IllegalStateException("there is no single element");
-  }
+
+    private int streams(int[] nums) {
+        Map<Integer, Integer> map = new HashMap<>();
+
+        IntStream.of(nums)
+                .forEach(num -> map.compute(num, (key, val) -> computeOccurrence(val)));
+
+        return map.entrySet().stream()
+                .filter(e -> e.getValue() == 1)
+                .map(e -> e.getKey())
+                .collect(toSingleton());
+    }
+
+    private Integer computeOccurrence(Integer val) {
+        return (val == null ? 1 : ++val);
+    }
+
+    private int hashMap(int[] nums) {
+        Map<Integer, Integer> map = new HashMap<>();
+        for (int n : nums) {
+            map.compute(n, (key, val) -> (val == null) ? 1 : ++val);
+        }
+
+        for (Entry<Integer, Integer> entry : map.entrySet()) {
+            if (entry.getValue() == 1) {
+                return entry.getKey();
+            }
+        }
+        return 0;
+    }
 }
--- src/test/java/cane/brothers/problems/single_element/SolutionTest.java
+++ src/test/java/cane/brothers/problems/single_element/SolutionTest.java
@@ -1,36 +1,30 @@
 package cane.brothers.problems.single_element;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-
-import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class SolutionTest {
 
-  @Test
-  void getSingleElement_one() {
-    int[] nums = new int[] {1, 2, 2, 3, 3};
-    Solution sol = new Solution();
-    assertEquals(1, sol.getSingleElement(nums), "wrong single element");
-  }
+    @Test
+    void getSingleElement_one() {
+        int[] nums = new int[]{1, 2, 2, 3, 3};
+        Solution sol = new Solution();
+        assertEquals(1, sol.getSingleElement(nums), "wrong single element");
+    }
 
-  @Test()
-  void getSingleElement_no_one() {
-    int[] nums = new int[] {1, 1, 2, 2, 3, 3};
-    Solution sol = new Solution();
-    IllegalStateException thrown = Assertions.assertThrows(IllegalStateException.class, () ->
-        sol.getSingleElement(nums)
-    );
-    assertEquals("there is no single element", thrown.getMessage());
-  }
+    @Test
+    void getSingleElement_uno() {
+        int[] nums = new int[]{2, 2, 1};
+        Solution sol = new Solution();
+        assertEquals(1, sol.getSingleElement(nums), "wrong single element");
+    }
 
-  @Test
-  void getSingleElement_all() {
-    int[] nums = new int[] {1, 2, 3};
-    Solution sol = new Solution();
-    IllegalStateException thrown = Assertions.assertThrows(IllegalStateException.class, () ->
-        sol.getSingleElement(nums)
-    );
-    assertEquals("there is no single element", thrown.getMessage());
-  }
+    @Test()
+    void getSingleElement_four() {
+        int[] nums = new int[]{4, 1, 2, 1, 2};
+        Solution sol = new Solution();
+        assertEquals(4, sol.getSingleElement(nums), "wrong single element");
+
+    }
 }
