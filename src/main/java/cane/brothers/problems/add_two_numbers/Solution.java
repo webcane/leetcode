6c08a13e56bc46070cec6c2205d8bd25ba640008
Mikhail Niedre <mikhail.niedre@quantori.com>
2026-02-04 22:38:56 +0500

    fix add_two_numbers

--- src/main/java/cane/brothers/problems/add_two_numbers/Solution.java
+++ src/main/java/cane/brothers/problems/add_two_numbers/Solution.java
@@ -2,40 +2,37 @@ package cane.brothers.problems.add_two_numbers;
 
 public class Solution {
     public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
-        ListNode result = new ListNode();
-        ListNode head = result;
+        return useWhile(l1, l2);
+    }
 
+    private ListNode useWhile(ListNode l1, ListNode l2) {
+        ListNode result = new ListNode(0);
+        ListNode cursor = result;
         int carry = 0;
 
-        while (l1 != null || l2 != null) {
-            int l1Val = getNodeValue(l1);
-            int l2Val = getNodeValue(l2);
+        ListNode ln1 = l1, ln2 = l2;
+        while (ln1 != null || ln2 != null || carry != 0) {
+            int l1Val = getNodeValue(ln1);
+            int l2Val = getNodeValue(ln2);
 
             int sum = l1Val + l2Val + carry;
+            carry = sum / 10;
+            int currVal = sum % 10;
+            cursor.next = new ListNode(currVal);
+            cursor = cursor.next;
 
-            carry = sum >= 10 ? 1 : 0;
-            sum = sum % 10;
-
-            result.next = new ListNode(sum);
-            result = result.next;
-
-            l1 = getNodeNext(l1);
-            l2 = getNodeNext(l2);
+            if (ln1 != null)
+                ln1 = ln1.next;
+            if (ln2 != null)
+                ln2 = ln2.next;
         }
 
-        if (carry != 0) {
-            result.next = new ListNode(carry);
-        }
+        return result.next;
 
-        return head.next;
     }
 
     protected int getNodeValue(ListNode node) {
         // Use value as 0 from the list that ended shorter than the other.
         return node != null ? node.val : 0;
-    }
-
-    protected ListNode getNodeNext(ListNode node) {
-        return node != null ? node.next : null;
     }
 }
--- src/test/java/cane/brothers/problems/add_two_numbers/SolutionTest.java
+++ src/test/java/cane/brothers/problems/add_two_numbers/SolutionTest.java
@@ -7,7 +7,7 @@ import org.junit.jupiter.api.Test;
 class SolutionTest {
 
   @Test
-  void addTwoNumbers() {
+  void test_addTwoNumbers_1() {
     ListNode node1 = new ListNode(2, new ListNode(4, new ListNode(3)));
     ListNode node2 = new ListNode(5, new ListNode(6, new ListNode(4)));
 
@@ -18,4 +18,22 @@ class SolutionTest {
     assertEquals(0, result.next.val);
     assertEquals(8, result.next.next.val);
   }
+
+    @Test
+    void test_addTwoNumbers_2() {
+        ListNode node1 = new ListNode(9, new ListNode(9, new ListNode(9, new ListNode(9, new ListNode(9, new ListNode(9, new ListNode(9)))))));
+        ListNode node2 = new ListNode(9, new ListNode(9, new ListNode(9, new ListNode(9))));
+
+        Solution sol = new Solution();
+        ListNode result = sol.addTwoNumbers(node1, node2);
+
+        assertEquals(8, result.val);
+        assertEquals(9, result.next.val);
+        assertEquals(9, result.next.next.val);
+        assertEquals(9, result.next.next.next.val);
+        assertEquals(0, result.next.next.next.next.val);
+        assertEquals(0, result.next.next.next.next.next.val);
+        assertEquals(0, result.next.next.next.next.next.next.val);
+        assertEquals(1, result.next.next.next.next.next.next.next.val);
+    }
 }
