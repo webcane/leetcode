145fa4a41aeca41bbbb04ce9c2e16b9d413580ea
Mikhail Niedre <mikhail.niedre@quantori.com>
2026-02-08 17:10:59 +0500

    partitionLabels usingPointer

--- src/main/java/cane/brothers/problems/partition_labels/Solution.java
+++ src/main/java/cane/brothers/problems/partition_labels/Solution.java
@@ -22,7 +22,9 @@ public class Solution {
 
     public List<Integer> partitionLabels(String s) {
         validateInput(s);
-        return classic(s);
+//        return usingMap(s);
+//        return classic(s);
+        return usingPointer(s);
     }
 
     /**
@@ -31,7 +33,59 @@ public class Solution {
      */
     private List<Integer> classic(String s) {
         var result = new ArrayList<Integer>();
-        // TODO
+
+        int[] last = new int[26];
+
+        // Шаг 1: Создаём словарь последних вхождений каждой буквы
+        for (int i = 0; i < s.length(); i++) {
+            last[s.charAt(i) - 'a'] = i;
+        }
+
+        int start = 0, end = 0;
+        // Шаг 2: Проходим по строке и формируем сегменты
+        for (int i = 0; i < s.length(); i++) {
+            end = Math.max(end, last[s.charAt(i) - 'a']);
+
+            // Если достигли конца сегмента
+            if (i == end) {
+                // Добавляем длину сегмента в результат
+                result.add(end - start + 1);
+                // Начинаем новый сегмент
+                start = i + 1;
+            }
+        }
+        return result;
+    }
+
+    private List<Integer> usingMap(String s) {
+        var result = new ArrayList<Integer>();
+        var last = new java.util.HashMap<Character, Integer>();
+        for (int i = 0; i < s.length(); i++) {
+            last.put(s.charAt(i), i);
+        }
+        int start = 0, end = 0;
+        for (int i = 0; i < s.length(); i++) {
+            end = Math.max(end, last.get(s.charAt(i)));
+            if (i == end) {
+                result.add(end - start + 1);
+                start = i + 1;
+            }
+        }
+        return result;
+    }
+
+    private List<Integer> usingPointer(String s) {
+        var result = new ArrayList<Integer>();
+
+        Pointer p = new Pointer(s);
+        for (int i = 0; i < s.length(); i++) {
+            p.extendSegment(s.charAt(i));
+
+            if (p.isSegmentFinished(i)) {
+                result.add(p.getSegmentLength());
+                p.startNewSegment(i);
+            }
+        }
         return result;
     }
 
@@ -41,5 +95,33 @@ public class Solution {
         }
 
         //
+    }
+
+    private static class Pointer {
+        private final int[] last = new int[26];
+        private int start = 0;
+        private int end = 0;
+
+        Pointer(String s) {
+            for (int i = 0; i < s.length(); i++) {
+                last[s.charAt(i) - 'a'] = i;
+            }
+        }
+
+        public int getSegmentLength() {
+            return end - start + 1;
+        }
+
+        public void startNewSegment(int i) {
+            start = i + 1;
+        }
+
+        public boolean isSegmentFinished(int i) {
+            return i == end;
+        }
+
+        public void extendSegment(char c) {
+            end = Math.max(end, last[c - 'a']);
+        }
     }
 }
