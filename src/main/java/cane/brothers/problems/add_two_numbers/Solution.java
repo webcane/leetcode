3d689f6274b95ab7541af4b006d88d90a18f4ac2
Mikhail Niedre <mikhail.niedre@quantori.com>
2026-02-08 17:23:55 +0500

    addTwoNumbers

--- src/main/java/cane/brothers/problems/add_two_numbers/Solution.java
+++ src/main/java/cane/brothers/problems/add_two_numbers/Solution.java
@@ -6,19 +6,26 @@ public class Solution {
         return useWhile(l1, l2);
     }
 
+    // сложения двух чисел, представленных в виде связанных списков,
+    // где каждый узел содержит одну цифру, а цифры записаны в обратном порядке (единицы в начале).
     private ListNode useWhile(ListNode l1, ListNode l2) {
         ListNode result = new ListNode(0);
         ListNode cursor = result;
         int carry = 0;
 
+        // Проходит по обоим спискам одновременно,
         while (l1 != null || l2 != null || carry != 0) {
             int l1Val = getNodeValue(l1);
             int l2Val = getNodeValue(l2);
 
+            // складывая соответствующие цифры и учитывая (carry).
             int sum = l1Val + l2Val + carry;
             carry = sum / 10;
             int currVal = sum % 10;
+
+            // текущее значение записываем в новый узел результирующего списка.
             cursor.next = new ListNode(currVal);
+            // обновляем курсор
             cursor = cursor.next;
 
             if (l1 != null)
@@ -27,6 +34,7 @@ public class Solution {
                 l2 = l2.next;
         }
 
+        // возвращаем результат отсекая ведущий 0
         return result.next;
 
     }
