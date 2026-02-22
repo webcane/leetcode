354db533a71281bd818672a37e3a496c6e27cf69
Mikhail Niedre <mikhail.niedre@quantori.com>
2026-02-23 00:29:12 +0500

    squares-of-a-sorted-array

--- src/main/java/cane/brothers/problems/squares_of_a_sorted_array/Solution.java
+++ src/main/java/cane/brothers/problems/squares_of_a_sorted_array/Solution.java
@@ -11,11 +11,40 @@ public class Solution {
 
     public int[] sortedSquares(int[] nums) {
         validateInput(nums);
+
+        return withSort(nums);
+//        return twoPointers(nums);
+    }
+
+    public int[] withSort(int[] nums) {
         var result = new int[nums.length];
         for(int i = 0; i< nums.length; i++) {
             result[i] = nums[i] * nums[i];
         }
         Arrays.sort(result);
+        return result;
+    }
+
+    public int[] twoPointers(int[] nums) {
+        int n = nums.length;
+        int[] result = new int[n];
+
+        int left = 0;
+        int right = n - 1;
+
+        for (int i = n - 1; i >= 0; i--) {
+            int leftSquare = nums[left] * nums[left];
+            int rightSquare = nums[right] * nums[right];
+
+            if (leftSquare > rightSquare) {
+                result[i] = leftSquare;
+                left++;
+            } else {
+                result[i] = rightSquare;
+                right--;
+            }
+        }
+
         return result;
     }
 
